381017ee020c3622086d3ffeb3236ad460649844
Michael Salam <michaeldimimu@gmail.com>
2024-09-09 19:12:11 +0100

    Create dummy ui for my events page

--- src/pages/private/my-events/index.jsx
+++ src/pages/private/my-events/index.jsx
@@ -1,5 +1,5 @@
-import { SubjectOutlined } from "@mui/icons-material";
 import { Link } from "react-router-dom";
+import MyEventItem from "./components/MyEventItem";
 
 const MyEvents = () => {
   return (
@@ -7,13 +7,22 @@ const MyEvents = () => {
       <h1>My events</h1>
       <p className="mt-2 mb-8">The events you have created will show up here</p>
 
-      <p className="text-xl">
-        No events yet!{" "}
-        <Link to="/create-event" className="text-accent underline">
-          Create an event
-        </Link>{" "}
-        to see it show up here.
-      </p>
+      {true ? (
+        <div className="grid gap-4 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <MyEventItem />
+          <MyEventItem />
+          <MyEventItem />
+          <MyEventItem />
+        </div>
+      ) : (
+        <p className="text-xl">
+          No events yet!{" "}
+          <Link to="/create-event" className="text-accent underline">
+            Create an event
+          </Link>{" "}
+          to see it show up here.
+        </p>
+      )}
     </main>
   );
 };
