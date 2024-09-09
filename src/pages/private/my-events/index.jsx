2ec9ae7190c5013ec5f8a5e626cef0fcc72c8793
Michael Salam <michaeldimimu@gmail.com>
2024-09-09 18:58:49 +0100

    Create empty state for my events page

--- src/pages/private/my-events/index.jsx
+++ src/pages/private/my-events/index.jsx
@@ -1,8 +1,19 @@
+import { SubjectOutlined } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+
 const MyEvents = () => {
   return (
     <main className="p-4">
       <h1>My events</h1>
-      <p className="mt-2">The events you have created will show up here</p>
+      <p className="mt-2 mb-8">The events you have created will show up here</p>
+
+      <p className="text-xl">
+        No events yet!{" "}
+        <Link to="/create-event" className="text-accent underline">
+          Create an event
+        </Link>{" "}
+        to see it show up here.
+      </p>
     </main>
   );
 };
